89db1798a24c3ba718870bfe0a19ead63348e2e4
Tacettin Kustu <tacettinkustu@gmail.com>
2022-01-29 08:20:15 +0300

    Set up db

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // MongoDB and mongoose connect
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
